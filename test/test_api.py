adb178e711f719696ab4367b3ab58d6fa9f512e1
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-07-10 18:49:51 +1200

    Finished enrollment tests

--- test/test_api.py
+++ test/test_api.py
@@ -3,6 +3,8 @@ import unittest, json
 from flask import Flask, session
 from flask_fido_u2f import U2F
 
+from .soft_u2f_v2 import SoftU2FDevice
+
 class APITest(unittest.TestCase):
     def setUp(self):
         self.app      = Flask(__name__)
@@ -11,9 +13,10 @@ class APITest(unittest.TestCase):
         self.app.config['SECRET_KEY'] = 'DjInNB3l9GBZq2D9IsbBuHpOiLI5H1iBdqJR24VPHdj'
         self.app.config['U2F_APPID']  = 'https://example.com'
 
-        self.u2f      = U2F(self.app)
-        self.u2f_keys = []
+        self.u2f       = U2F(self.app)
+        self.u2f_keys  = []
 
+        self.u2f_token = SoftU2FDevice()
 
         @self.u2f.read
         def read():
@@ -45,7 +48,7 @@ class APITest(unittest.TestCase):
             'error'  : 'Unauthorized!'
         })
 
-    # ----- Checking GET enroll ----- #
+    # ----- Checking GET enroll structure ----- #
 
         with self.client as c:
             with c.session_transaction() as sess:
@@ -55,9 +58,8 @@ class APITest(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.headers['Content-Type'], 'application/json')
-        response_json = json.loads(response.get_data(as_text=True))
 
-        print(response_json)
+        response_json = json.loads(response.get_data(as_text=True))
 
         enroll_seed_model = {
             'status'               : str,
@@ -78,6 +80,48 @@ class APITest(unittest.TestCase):
         self.assertTrue(all(type(response_json['registerRequests'][0][key]) == enroll_seed_model_registerRequests[key] for key in enroll_seed_model_registerRequests.keys()))
         
         self.assertEqual(response_json['registerRequests'][0]['version'], 'U2F_V2')
+
+    # ----- Verifying enroll ----- #
+    
+      # ----- 400 BAD REQUEST ----- #
+        challenge = response_json['registerRequests'][0]
+        keyhandle = self.u2f_token.register(challenge)
+
+        response = self.client.post('/enroll', data=json.dumps(keyhandle), headers={"content-type": "application/json"})
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+
+        response_json = json.loads(response.get_data(as_text=True))
+
+        self.assertDictEqual(response_json, {
+            'error': 'Invalid key handle!', 
+            'status': 'failed'
+        })
+
+    # ----- 201 CREATED ----- #
+
+        response = self.client.get('/enroll')
+        response_json = json.loads(response.get_data(as_text=True))
+        
+        challenge = response_json['registerRequests'][0]
+
+        keyhandle = self.u2f_token.register(challenge, facet=self.app.config['U2F_APPID'])
+
+        response = self.client.post('/enroll', data=json.dumps(keyhandle), headers={"content-type": "application/json"})
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.headers['Content-Type'], 'application/json')
+
+        response_json = json.loads(response.get_data(as_text=True))
+
+        self.assertDictEqual(response_json, {
+            'status'  : 'ok', 
+            'message' : 'Successfully enrolled new U2F device!'
+        })
+
+
+
 
     def tearDown(self):
         pass
