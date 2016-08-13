c776e7d6b7bd3be0852d66377591b5afe27dd017
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-08-14 00:02:12 +1200

    Bumped version up. Updated setup py

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ from setuptools import setup
 
 setup(
     name                 = 'Flask-FIDO-U2F',
-    version              = '0.3.1',
+    version              = '0.4.0',
     url                  = 'https://github.com/herrjemand/flask-fido-u2f',
     license              = 'MIT',
     author               = 'Ackermann Yuriy',
     author_email         = 'ackermann.yuriy@gmail.com',
     description          = 'A Flask plugin that adds FIDO U2F support.',
-    long_description     = __doc__,
+    long_description     =  read('README.md'),
+    keywords             = 'flask fido u2f 2fa',
 
     py_modules           = ['flask_fido_u2f'],
     zip_safe             = True,
@@ -28,10 +29,23 @@ setup(
     ],
     classifiers          = [
         'Environment :: Web Environment',
-        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        # Needs python 2.x testing
+        # 'Programming Language :: Python :: 2',
+        # 'Programming Language :: Python :: 2.6',
+        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
