e0b30bba2fabf9269e37ff1bc4d93cfbf40b03a4
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-08-14 00:08:35 +1200

    Missed function

--- setup.py
+++ setup.py
@@ -4,7 +4,17 @@ Flask-FIDO-U2F
 
 Flask plugin to simplify usage and management of U2F devices.
 """
+import os
 from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+# https://pythonhosted.org/an_example_pypi_project/setuptools.html
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name                 = 'Flask-FIDO-U2F',
