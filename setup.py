ed2cc233df23452e63e7010ffb9268c2438566c9
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2016-08-15 16:30:23 +1200

    Bumped version up.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name                 = 'Flask-FIDO-U2F',
-    version              = '0.4.1',
+    version              = '0.4.2',
     url                  = 'https://github.com/herrjemand/flask-fido-u2f',
     license              = 'MIT',
     author               = 'Ackermann Yuriy',
